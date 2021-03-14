60e2a773d12517610072c1ed2f7fa03c7cd5976d
ArisNurAmrozi <aris.nura03@gmail.com>
2021-03-14 19:43:56 +0700

    kuis1

---
+++ Kuis1/Assets/script/lompat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lompat : MonoBehaviour{
+ 
+    public float jump;
+    bool berdiri = true;
+    public bool Floor;
+    public Transform Detection;
+    public LayerMask Layer_Sellection;
+    public float Radius;
+
+    void Update(){
+
+        Floor = Physics2D.OverlapCircle(Detection.position, Radius, Layer_Sellection);
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && Floor){
+            if(berdiri){
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+
+            }
+        }
+    }
+}
